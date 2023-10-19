cbe8080a3e331648d24845ef86db9c74ee6f1c5b
Ben <behnam.iam@gmail.com>
2023-10-19 19:50:11 +0100

    3-23-react component basics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+const products = [
+  {name: 'product1', price: 100.00},
+  {name: 'product2', price: 200.00},
+  {name: 'product3', price: 300.00},
+  {name: 'product3', price: 300.00},
+]
 
 function App() {
 
   return (
     <div>
       <h1>ReStore</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
