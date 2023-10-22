c9cfa3af8dae39532fe213ed0ea296ad8b530eca
Ben <behnam.iam@gmail.com>
2023-10-22 10:27:11 +0100

    3-27-Adding a typescript interface for Product

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
-const products = [
-  {name: 'product1', price: 100.00},
-  {name: 'product2', price: 200.00},
-  {name: 'product3', price: 300.00},
-  {name: 'product3', price: 300.00},
-]
+import { useEffect, useState } from "react"
+import { Product } from "./product"
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([])
 
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, []) // this empty array is important it prevents this call from being rerendered over and over again...
+  // forgetting it will lead to an endless loop calls to the server
+
+  function addProduct(){
+    setProducts(prevState => [...prevState, {
+      id: prevState.length +1,
+      name: 'product' + (prevState.length + 1), 
+      price:(prevState.length*2),
+      brand: 'some brand name',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photo/200'
+    }])
+  }
   return (
     <div>
       <h1>ReStore</h1>
@@ -15,8 +29,10 @@ function App() {
           <li key={index}>{product.name} - {product.price}</li>
         ))}
       </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   )
 }
 
 export default App
+// react hooks are to create component state
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,5 +6,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>, 
+  //a feature of strict mode is, it makes two reuqests in developmetn mode to any api resources for verification purposes
+);
