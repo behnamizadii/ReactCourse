87b7a1d37417b0aa96174825e71222c0d0517deb
Ben <behnam.iam@gmail.com>
2023-10-26 17:43:24 +0100

    3-35-Styling the product card

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,7 +2,8 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function Header() {
   return(
-    <AppBar position='static'>
+    <AppBar position='static' sx={{mb: 4}}> 
+    {/* // check the app bar api in material ui for more on what is sx and mb */}
       <Toolbar>
         <Typography variant="h6">
           ReStore
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Avatar, Grid, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 
@@ -8,10 +8,12 @@ interface Props {
 
 export default function ProductList({products}: Props) { // this is called destructuring you can specify what you want instead of props
   return(
-    <List>
+    <Grid container spacing={4}>
         {products.map(product => ( //by destructuring here I don't even need props.products I can just use 'products'
+        <Grid item xs={3}>
           <ProductCard key={product.id} product={product} />
+        </Grid>
         ))}
-      </List>
+    </Grid>
   )
 }
