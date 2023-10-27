3b7095a03d9c584dbbf766f176f25d8483912ef9
Ben <behnam.iam@gmail.com>
2023-10-27 11:50:02 +0100

    cleanup

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,36 +1,15 @@
-import { useEffect, useState } from "react"
-import { Product } from "../models/product"
 import Catalog from "../../features/catalog/Catalog"
 import { CssBaseline, Container } from "@mui/material"
 import Header from "./Header"
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, []) // this empty array is important it prevents this call from being rerendered over and over again...
-  // forgetting it will lead to an endless loop calls to the server
-
-  function addProduct(){
-    setProducts(prevState => [...prevState, {
-      id: prevState.length +1,
-      name: 'product' + (prevState.length + 1), 
-      price:(prevState.length*2),
-      brand: 'some brand name',
-      description: 'some description',
-      pictureUrl: 'http://picsum.photo/200'
-    }])
-  }
   return (
     <div>
       {/* this helps the header to be edge to edge without any margin or paddings */}
       <CssBaseline />  
       <Header />
       <Container>
-        <Catalog products = {products} addProduct = {addProduct}/>
+        <Catalog />
       </Container>
       
     </div>
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,17 +1,20 @@
-import { Button } from "@mui/material";
 import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
 
-interface Props {
-  products: Product[];
-  addProduct: () => void;
-}
-
-export default function Catalog({products, addProduct}: Props) { // this is called destructuring you can specify what you want instead of props: Props which imports everything
+export default function Catalog() { // this is called destructuring you can specify what you want instead of props: Props which imports everything
+    const [products, setProducts] = useState<Product[]>([])
+  
+    useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    }, []) // this empty array is important it prevents this call from being rerendered over and over again...
+    // forgetting it will lead to an endless loop calls to the server
+  
   return (
     <>
       <ProductList products={products} />
-      <Button variant="contained" onClick={addProduct}>Add Product</Button>
       </>
   )
 }
