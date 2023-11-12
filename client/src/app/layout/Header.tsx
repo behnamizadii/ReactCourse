76de7165eea8c3cb45950aa1e9afce30a8e34d8f
Ben <behnam.iam@gmail.com>
2023-11-12 21:31:51 +0000

    4-42. Stying the nav bar

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,67 +1,88 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 const midlinks = [
-  {title: 'catalog', path: '/catalog'},
-  {title: 'about', path:'/about'},
-  {title: 'contact', path:'/contact'}
+  { title: 'catalog', path: '/catalog' },
+  { title: 'about', path: '/about' },
+  { title: 'contact', path: '/contact' }
 ]
 
 const rightLinks = [
-  {title: 'login', path: '/login'},
-  {title: 'register', path:'/register'}
+  { title: 'login', path: '/login' },
+  { title: 'register', path: '/register' }
 ]
 
-
-interface Props{
-  darkMode: boolean;
-  handleChange: ()=> void;
+const navLinkStyle = {
+  color: 'inherit',
+  typography: 'h6',
+  textdecoration: 'none',
+  '&:hover': {
+    color: 'grey.500'
+  },
+  '&.active': {
+    color: 'text.secondary'
+  }
 }
 
-export default function Header({darkMode, handleChange}: Props) {
-  return(
-    <AppBar position='static' sx={{mb: 4}}> 
-    {/* // check the app bar api in material ui for more on what is sx and mb */}
-      <Toolbar>
-        <Typography variant="h6" component={NavLink} to={'/'}
-        sx={{color:'inherit', textDecoration:'none'}}>
-          ReStore
-        </Typography>
-        <Switch 
-        checked={darkMode}
-        onChange={handleChange} />
-        <List sx={{display: 'flex'}}>
-          {midlinks.map(({title, path})=>(
+
+interface Props {
+  darkMode: boolean;
+  handleChange: () => void;
+}
+
+export default function Header({ darkMode, handleChange }: Props) {
+  return (
+    <AppBar position='static' sx={{ mb: 4 }}>
+      {/* // check the app bar api in material ui for more on what is sx and mb */}
+      <Toolbar sx={{ justifyContent: 'space-between', display: 'flex', alignItems: 'center' }}>
+        <Box display='flex' alignItems='center'>
+          <Typography
+            variant='h6'
+            component={NavLink}
+            to='/'
+            sx={navLinkStyle}>
+            RE-STORE
+          </Typography>
+          <Switch
+            checked={darkMode}
+            onChange={handleChange} />
+        </Box>
+
+        <List sx={{ display: 'flex' }}>
+          {midlinks.map(({ title, path }) => (
             <ListItem
               component={NavLink}
               to={path}
               key={path}
-              sx={{color:'inherit', typography:'h6'}}
+              sx={navLinkStyle}
             >
               {title.toUpperCase()}
             </ListItem>
           ))}
         </List>
 
-        <IconButton size='large' edge='start' color='inherit' sx={{mr:2 }}>
+        <Box display='flex' alignItems='center'>
+          <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
             <Badge badgeContent='4' color='secondary'>
               <ShoppingCart />
             </Badge>
-        </IconButton>
+          </IconButton>
 
-        <List sx={{display: 'flex'}}>
-          {rightLinks.map(({title, path})=>(
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={path}
-              sx={{color:'inherit', typography:'h6'}}
-            >
-              {title.toUpperCase()}
-            </ListItem>
-          ))}
-        </List>
+          <List sx={{ display: 'flex' }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem
+                component={NavLink}
+                to={path}
+                key={path}
+                sx={navLinkStyle}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
       </Toolbar>
     </AppBar>
   )
